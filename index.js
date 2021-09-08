4793ac29a665db7f8eeee4c6955e0d35c7e14fae
maja-wright <maja.filipasic@hotmail.com>
2021-09-08 14:58:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+app.get('/about', (req, res) => {
+    res.render("about");
+});
+
+app.get('/projects', (req, res) => {
+    res.render("projects");
+});
+
+app.get('/contact', (req, res) => {
+    res.render("contact");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+});
